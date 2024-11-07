3e7099b74b9be5fd143ef83bcef24c01febf0dd0
Charles L <charlesliu618@gmail.com>
2024-11-07 10:26:50 -0600

    Adding documentation

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = function(){
+    // When the window is loaded, get the date that the website need to track down.
     const finalDate = new Date("Dec 23, 2024 23:59:59")
 
+    // Actual counting down function for the time counting down.
+    // Taking two parameters,first is the anonimous function, second is the time duration of the function.
     const countDown = setInterval(() => {
 
+        // Get the current time
         const currentTime = new Date()
 
+        // Calculate the difference between the current time and the Final time.
         const totalCountDown = finalDate - currentTime
 
         //Calculate the actual days,hours,mins,secs from now to the final day.
@@ -14,12 +19,13 @@ window.onload = function(){
         const seconds = Math.floor((totalCountDown % (1000 * 60)) / 1000);
 
 
-        //update the inner HTML
+        //Navigating and save the elements of day, hours, minutes, seconds to the variables
         const DayEle = document.getElementById("days") 
         const HourEle = document.getElementById("hours") 
         const MinEle = document.getElementById("minutes") 
         const SecEle = document.getElementById("seconds")
 
+        // Change their innerHTML values to the updated value
         DayEle.innerHTML = day +  " <p id='inner_days'>Days</p><br>"
         HourEle.innerHTML = hours + " <span>Hours</span>"
         MinEle.innerHTML = minutes + " <span>Min</span>"
@@ -30,6 +36,8 @@ window.onload = function(){
         // addAnimation(DayEle);
         // addAnimation(HourEle);
         // addAnimation(MinEle);
+
+        // Applied the animation to the elements
         addAnimation(SecEle);
         addAnimation(title)
         addAnimation(className)
@@ -37,7 +45,8 @@ window.onload = function(){
     },1000);
 
     
-
+    // This is the add animation function, the main task for this function is to add the animaiton class to the element from the parameter, then remove is after 
+    // 300 miliseconds.
     function addAnimation(element){
         element.classList.add("changeAnimation");
         setTimeout(()=>{
